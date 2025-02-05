388eac3e7e9704dd23d8a938016241cdf881098d
kandalog <asuasu.tee@gmail.com>
2025-02-05 09:36:55 +0900

    typingゲームロジック追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,75 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+
+const question = [
+  { question: "React", image: "./monster1.jpg" },
+  { question: "TypeScript", image: "./monster2.jpg" },
+  { question: "JISOU", image: "./monster3.jpg" },
+  { question: "GitHub", image: "./monster4.jpg" },
+  { question: "Next.js", image: "./monster5.jpg" },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [isCompleted, setIsCompleted] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const currentQuestion = question[currentQuestionIndex];
+      if (
+        e.key.toLowerCase() ===
+        currentQuestion.question[currentPosition].toLowerCase()
+      ) {
+        setCurrentPosition((prev) => prev + 1);
+      }
+      if (currentPosition === currentQuestion.question.length - 1) {
+        if (currentQuestionIndex === question.length - 1) {
+          setIsCompleted(true);
+        } else {
+          setCurrentQuestionIndex((prev) => prev + 1);
+          setCurrentPosition(0);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [currentQuestionIndex, currentPosition, isCompleted]);
+
+  if (isCompleted) {
+    return <div>ゲーム終了</div>;
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div
+        className="text-center w-full h-screen gb-cover gb-center flex flex-col items-center justify-center"
+        style={{
+          backgroundImage: `url(${question[currentQuestionIndex].image})`,
+          backgroundColor: "rgba(0,0,0,0.5",
+          backgroundBlendMode: "overlay",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div>
+          {question[currentQuestionIndex].question
+            .split("")
+            .map((char, index) => (
+              <span
+                key={index}
+                style={{ color: index < currentPosition ? "#ff0000" : "#fff" }}
+              >
+                {char}
+              </span>
+            ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
